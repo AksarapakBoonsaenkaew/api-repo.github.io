e164af226f901365c01c0d1a7134f651979a7f42
aksarapakboonsaenkaew <aks.fer@kkumail.com>
2024-06-16 23:00:48 +0700

    add 23.00 16.6.67

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-const request = new XMLHttpRequest();
-request.open("GET","https://aksarapakboonsaenkaew.github.io/api-repo.github.io/api-test.json")
-request.send();
-request.onload = ()=>{
-    console.log(request);
-} 
+const apiUrl = 'https://aksarapakboonsaenkaew.github.io/api-repo.github.io/api-test.json';
+
+const contactForm = document.getElementById('contact-form');
+const responseMessage = document.getElementById('response-message');
+
+contactForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const formData = new FormData(contactForm);
+
+  const requestOptions = {
+    method: 'POST',
+    body: formData,
+  };
+
+  fetch(apiUrl, requestOptions)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.text();
+    })
+    .then(data => {
+      responseMessage.textContent = data;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+});
